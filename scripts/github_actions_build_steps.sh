4795bf1ea0caba1357648bfe2d6112e3c3cc79cb
Yakovyakov <yyak0423@gmail.com>
2025-02-20 12:00:45 -0500

    fixing pipeline

---
+++ scripts/github_actions_build_steps.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "Build script"
+
+# Step 1: Install frontend dependencies
+echo "Installing frontend dependencies..."
+cd frontend
+npm ci --also=dev
+if [ $? -ne 0 ]; then
+  echo "Error: Installing frontend dependencies...failed"
+  exit 1
+fi
+echo "Installing frontend dependencies...OK"
+# Step 2: Install backend dependencies
+echo "Installing backend dependencies..."
+
+cd ../backend
+
+npm install
+if [ $? -ne 0 ]; then
+  echo "Error: Installing backend dependencies...failed"
+  exit 1
+fi
+echo "Installing backend dependencies...OK"
+
+cd ..
+# Step 3: building
+echo "Building..."
+npm run build
+if [ $? -ne 0 ]; then
+  echo "Error: Building...failed"
+  exit 1
+fi
+echo "Building...OK"
+
+echo "Build script finish"
+
+
