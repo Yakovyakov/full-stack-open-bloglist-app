7de6cd19f480ee91ced94af606e02fc4752e41a9
Yakovyakov <yyak0423@gmail.com>
2025-02-24 11:54:37 -0500

    Improving the backend #SKIP
    handle an route not handle by the backend
    
    improving new health

--- backend/controllers/health.js
+++ backend/controllers/health.js
@@ -1,8 +1,14 @@
 const healthRouter = require('express').Router()
+const packageJson = require('../../package.json')
 
+// eslint-disable-next-line no-unused-vars
 healthRouter.get('/', async (request, response) => {
 
-  response.send('OK')
+  response.json({
+    status: 'OK',
+    version: packageJson.version,
+    timestamp: new Date().toISOString()
+  })
 })
 
 module.exports = healthRouter
